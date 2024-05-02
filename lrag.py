2e50672eabe5ff81ff7fc7b7bbc3617a848775e4
Aminat Oluwakemi Owodunni <81017006+amiegirl@users.noreply.github.com>
2024-05-02 11:36:27 +0100

    app

--- lrag.py
+++ lrag.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import streamlit as st
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.agents import AgentType, create_sql_agent
 from langchain.sql_database import SQLDatabase
